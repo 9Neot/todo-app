0728e96261d11bef7ef513ddfc39c9a029b8d7bc
9Neot <bestoflong123@gmail.com>
2022-11-27 17:11:30 +0700

    useState hook -> useReducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useRef } from "react";
-import { useState } from "react";
+import React, { useCallback, useRef, useReducer, useState } from "react";
 import "./App.css";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
@@ -23,16 +22,65 @@ export interface IButton {
   searchButton: boolean;
 }
 
+type todoAction =
+  | {
+      type: "add";
+      payload: {
+        value: string;
+      };
+    }
+  | {
+      type: "delete";
+      payload: {
+        id: string;
+      };
+    }
+  | {
+      type: "toggle";
+      payload: {
+        id: string;
+      };
+    }
+  | {
+      type: "fetchData";
+      payload: ITodoList;
+    };
+
+const initialTodoList: ITodoList = [];
 const TODOLIST_STORAGE = "todoStorage";
 
+const todoReducer = (todoList: ITodoList, action: todoAction) => {
+  switch (action.type) {
+    case "add":
+      return [
+        ...todoList,
+        { id: uuidv4(), todoName: action.payload.value, isCompleted: false },
+      ];
+    case "delete":
+      return todoList.filter(todo => todo.id !== action.payload.id);
+    case "toggle": {
+      return todoList.map(todo => {
+        if (todo.id !== action.payload.id) {
+          return todo;
+        }
+        return { ...todo, isCompleted: !todo.isCompleted };
+      });
+    }
+    case "fetchData": {
+      return [...action.payload];
+    }
+    default:
+      return todoList;
+  }
+};
+
 function App() {
-  const [todoList, setTodoList] = useState<ITodoList>([]);
   const [selectedButton, setSelectedButton] = useState<IButton>({
     leftButton: null,
     rightButton: null,
     searchButton: false,
   });
-
+  const [todoList, todoDispatch] = useReducer(todoReducer, initialTodoList);
   const inputRef = useRef<HTMLInputElement | null>(null);
   const todoRef = useRef<HTMLUListElement | null>(null);
   const firstRender = useRef(true);
@@ -71,10 +119,10 @@ function App() {
   }, [selectedButton.rightButton, todoList]);
 
   useEffect(() => {
-    const newList = JSON.parse(
+    const newList: ITodoList = JSON.parse(
       localStorage.getItem(TODOLIST_STORAGE) as string
     );
-    setTodoList(newList);
+    todoDispatch({ type: "fetchData", payload: newList });
   }, []);
 
   useEffect(() => {
@@ -100,25 +148,18 @@ function App() {
       const value = (e.target as HTMLInputElement).value;
 
       if (value) {
-        setTodoList(pre => [
-          ...pre,
-          { id: uuidv4(), todoName: value, isCompleted: false },
-        ]);
+        todoDispatch({ type: "add", payload: { value: value } });
       }
       (e.target as HTMLInputElement).value = "";
     }
   };
 
   const handleToggleTodo = (id: string) => {
-    const newList = [...todoList];
-    const todo = newList.find(todo => todo.id === id);
-    todo!.isCompleted = !todo!.isCompleted;
-    setTodoList(newList);
+    todoDispatch({ type: "toggle", payload: { id: id } });
   };
 
   const handleDeleteTodo = (id: string) => {
-    const newList = todoList.filter(todo => todo.id !== id);
-    setTodoList(newList);
+    todoDispatch({ type: "delete", payload: { id: id } });
   };
 
   const handleSearchTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
